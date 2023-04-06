6af56498f0897421600ffd52fa52a0d211603a11
krkyk <100661771+krkyk@users.noreply.github.com>
2023-04-06 15:56:32 +0900

    デザインと基本機能作成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,114 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+/** @type {string} 現在の入力値　*/
+let currentNumber = "";
+/** @type {string} 演算子　*/
+let operator = "";
+/** @type {null|number} 演算子入力前の入力値　*/
+let leftValue = null;
+/** @type {null|Element} 入力値や演算結果の表示要素　*/
+let displayElement = null;
+
+// 数字ボタンを押したときの処理
+const numpudElements = document.querySelectorAll(".numpad");
+numpudElements.forEach((numpudElement) => {
+  numpudElement.addEventListener("click", (e) => {
+    handleNumpad(e.currentTarget.textContent);
+  });
+});
+
+// 演算子ボタンを押したときの処理
+const operatorElements = document.querySelectorAll(".operator");
+operatorElements.forEach((operatorElement) => {
+  operatorElement.addEventListener("click", (e) =>
+    handleOperator(e.currentTarget.textContent)
+  );
+});
+
+// クリアボタンを押したときの処理
+const clearAllElement = document.getElementById("clear-all");
+clearAllElement.addEventListener("click", () => handleClearAll());
+
+// 数字の入力処理
+const handleNumpad = (num) => {
+  if (currentNumber.length === 0 && num === "0") {
+    return;
+  }
+  currentNumber += num;
+  // console.log("handleNumpad");
+  // console.log(currentNumber);
+
+  console.log(leftValue);
+  console.log(operator);
+  console.log(currentNumber);
+
+  updateDisplay();
+};
+
+// 演算子の処理
+const handleOperator = (operator) => {
+  if (operator === "=" && leftValue && currentNumber) {
+    console.log("qqq");
+
+    const ret = calc();
+    leftValue = ret;
+    updateDisplay();
+  } else if (operator && operator !== "=" && currentNumber && !leftValue) {
+    console.log("yyy");
+
+    leftValue = currentNumber;
+    currentNumber = "";
+    updateDisplay();
+
+    console.log(leftValue);
+    console.log(operator);
+    console.log(currentNumber);
+  } else if ((!operator && leftValue) || currentNumber) {
+    console.log("lll");
+
+    leftValue = leftValue || parseFloat(currentNumber);
+  }
+
+  // 左辺に対するオペレーターを保存
+  operator = operator === "=" ? null : operator;
+  currentNumber = "";
+  updateDisplay();
+};
+
+// 計算処理
+const calc = () => {
+  const rightValue = parseFloat(currentNumber);
+  // console.log(rightValue);
+  // console.log(leftValue);
+
+  switch (operator) {
+    case "+":
+      return leftValue + rightValue;
+    case "-":
+      return leftValue - rightValue;
+    case "×":
+      return leftValue * rightValue;
+    case "÷":
+      return leftValue / rightValue;
+    default:
+      throw new Error(`Unsupported operator ${operator}`);
+  }
+};
+
+// すべて削除
+const handleClearAll = () => {
+  currentNumber = "";
+  operator = null;
+  leftValue = null;
+  // console.log("handleClearAll");
+
+  updateDisplay();
+};
+
+// 表示の更新
+const updateDisplay = () => {
+  displayElement = currentNumber || leftValue || "0";
+  document.getElementById("display").innerText = displayElement;
+
+  // console.log(displayElement);
+};
